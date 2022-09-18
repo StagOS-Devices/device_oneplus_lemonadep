b33fca8b0e3c015216bd9f07a90c1880d76b4d8f
LuK1337 <priv.luk@gmail.com>
2022-09-18 14:17:03 +0200

    lemonadep: We now run at 1080p by default ^.^
    Change-Id: I4989e95ff6d8b99413c3cffe5c7ec76ca9de9dff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,13 +9,15 @@ include device/oneplus/sm8350-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/oneplus/lemonadep
 
+# Display
+TARGET_SCREEN_DENSITY := 450
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
-TARGET_RECOVERY_DENSITY := xxhdpi
-TARGET_RECOVERY_UI_MARGIN_HEIGHT := 126
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/lemonadep/BoardConfigVendor.mk
